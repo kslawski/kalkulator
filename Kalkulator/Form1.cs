eed0ba550deb8779e4711c019b3a95b84a4f3506
kslawski <krzysiek.slawski@gmail.com>
2018-04-05 20:55:37 +0200

    Add files via upload

---
+++ Kalkulator/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "0")
+                textBox_result.Clear;
+
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+        }
+    }
+}
